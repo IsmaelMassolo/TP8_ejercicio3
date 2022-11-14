c3a8bf6c33117b8f4588f1f8278040f96f5020d3
IsmaelMassolo <ismaelmassolo@gmail.com>
2022-11-13 21:23:08 -0300

    first commit

---
+++ TP8_ej3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+/*3) Desarrollar un programa que solicite al usuario ingresar 6 números y almacenar en ARRAY y
+por último se lo debe recorrer para calcular y mostrar el promedio de los valores.*/
+namespace TP8C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            int [] nuevoArray = new int [6];
+            string paraGuardar = "";
+            int valor = 0;
+            int promedio = 0;
+            for (int i = 0; i < nuevoArray.Length; i++)
+            {
+                Console.WriteLine("Ingrese el "+(i+1)+"° numero entero...");
+                paraGuardar = Console.ReadLine ();
+                valor = Convert.ToInt32(paraGuardar); 
+                nuevoArray [i] = valor;
+                Console.Clear();
+            }
+            Console.WriteLine("Los datos ingresados son: ");
+            for (int i = 0; i < nuevoArray.Length; i++)
+            {
+                Console.WriteLine("Dato "+(i+1)+": "+nuevoArray[i]);
+                promedio = promedio + nuevoArray [i];
+            }
+            Console.WriteLine("Has terminado. El promedio de los numeros ingresado es: "+ promedio/nuevoArray.Length);
+            Console.WriteLine("Presione una tecla para continuar");
+            Console.ReadKey();
+        }
+    }
+}
